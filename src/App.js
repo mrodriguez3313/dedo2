f2f135edf499a0d4a02e695b8232974caea1fa35
Marco Rodriguez <marco.rodriguez113@gmail.com>
2021-04-22 15:01:31 -0700

    added feedDac, create posts, load posts

--- src/App.js
+++ src/App.js
@@ -7,34 +7,20 @@ import WorkshopForm from './components/Form';
 // Import UI Components
 import { Header, Tab, Container } from 'semantic-ui-react';
 
-/************************************************/
-/*        Step 4.2 Code goes here               */
-/************************************************/
-import { ContentRecordDAC } from '@skynetlabs/content-record-library';
+// Dac 
 import { FeedDAC } from "feed-dac-library";
+import { Posts } from "feed-dac-library/dist/cjs/skystandards";
 
-/*****/
-
-/************************************************/
-/*        Step 1.2 Code goes here               */
-/************************************************/
-// Import the SkynetClient and a helper
+// mysky
 import { SkynetClient } from 'skynet-js';
-
-// We'll define a portal to allow for developing on localhost.
-// When hosted on a skynet portal, SkynetClient doesn't need any arguments.
 const portal = 'https://siasky.net/';
 
 // Initiate the SkynetClient
 const client = new SkynetClient(portal);
-/*****/
 
-/************************************************/
-/*        Step 4.3 Code goes here               */
-/************************************************/
-const contentRecord = new ContentRecordDAC();
-// const feedDAC = new FeedDAC();
-/*****/
+
+const feedDAC = new FeedDAC();
+let posts = [];
 
 function App() {
   // Define app state helpers
@@ -61,20 +47,11 @@ function App() {
     setFilePath(dataDomain + '/' + dataKey);
   }, [dataKey]);
 
-  /************************************************/
-  /*        Step 3.1 Code goes here               */
-  /************************************************/
-
   // choose a data domain for saving files in MySky
   const dataDomain = 'localhost';
 
-  /*****/
-
   // On initial run, start initialization of MySky
   useEffect(() => {
-    /************************************************/
-    /*        Step 3.2 Code goes here               */
-    /************************************************/
     // define async setup function
     async function initMySky() {
       try {
@@ -83,7 +60,7 @@ function App() {
         const mySky = await client.loadMySky(dataDomain, {debug: true, dev:true});
 
         // load necessary DACs and permissions
-        await mySky.loadDacs(contentRecord);
+        await mySky.loadDacs(feedDAC);
 
         // check if user is already logged in with permissions
         const loggedIn = await mySky.checkLogin();
@@ -111,54 +88,42 @@ function App() {
     console.log('form submitted');
     setLoading(true);
 
-    /************************************************/
-    /*        Part 1: Upload a file                */
-    /************************************************/
     console.log('Uploading file...');
 
-    /************************************************/
-    /*        Step 1.3 Code goes here               */
-    /************************************************/
+    const res = feedDAC.createPost({
+      video: file,
+      name: name,
+      link: fileSkylink,
+      description: description,
+    });
 
-    // const res = feedDAC.createPost({
-    //   video: file,
-    // });
-    // if (res.success){
-    //   console.log(`Reference to the new video: ${res.ref}`);
-    // } else {
-    //   alert(`Error in submit: ${res.error}`);
-    // }
+    if (res.success) {
+      console.log(`Reference to the new video: ${res.ref}`);
+    } else {
+      alert(`Error in submit: ${res.error}`);
+    }
 
 
     // Upload user's file and get backs descriptor for our Skyfile
-    const { skylink } = await client.uploadFile(file);
+    // const { skylink } = await client.uploadFile(file);
 
     // skylinks start with 'sia://' and don't specify a portal URL
     // we can generate URLs for our current portal though.
-    const skylinkUrl = await client.getSkylinkUrl(skylink);
+    // const skylinkUrl = await client.getSkylinkUrl(skylink);
 
-    console.log('File Uploaded:', skylinkUrl);
+    // console.log('File Uploaded:', skylinkUrl);
 
     // To use this later in our React app, save the URL to the state.
-    setFileSkylink(skylinkUrl);
+    // setFileSkylink(skylinkUrl);
 
     /************************************************/
     /*        Part 3: MySky                         */
     /************************************************/
     console.log('Saving user data to MySky file...');
 
-    /************************************************/
-    /*        Step 3.6 Code goes here              */
-    /************************************************/
-    // create JSON data pertaining to file submitted to write to MySky
-    // the file will automatically be attached to userID
-    const jsonData = {
-      name,
-      description,
-    };
-
     // call helper function for MySky Write
-    await handleMySkyWrite(jsonData);
+    // await handleMySkyWrite(jsonData);
+    setLoading(false);
   };
 
   const handleMySkyLogin = async () => {
@@ -190,21 +155,21 @@ function App() {
     /*****/
   };
 
-  const handleMySkyWrite = async (jsonData) => {
-    /************************************************/
-    /*        Step 3.7 Code goes here               */
-    /************************************************/
-    // Use setJSON to save the user's information to MySky file
-    try {
-      console.log('userID', userID);
-      console.log('filePath', filePath);
-      await contentRecord.recordNewContent({ fileSkylink , metadata: {"ts":"", "fs": "3", "ft": "mp4", "ot": "Will make object later", "l": 0, "rp": 0}});
-    } catch (error) {
-      console.log(`error with setJSON: ${error.message}`);
-    }
-    setLoading(false);
+  // const handleMySkyWrite = async (jsonData) => {
+  //   /************************************************/
+  //   /*        Step 3.7 Code goes here               */
+  //   /************************************************/
+  //   // Use setJSON to save the user's information to MySky file
+  //   try {
+  //     console.log('userID', userID);
+  //     console.log('filePath', filePath);
+  //     await contentRecord.recordNewContent({ fileSkylink , metadata: {"ts":"", "fs": "3", "ft": "mp4", "ot": "Will make object later", "l": 0, "rp": 0}});
+  //   } catch (error) {
+  //     console.log(`error with setJSON: ${error.message}`);
+  //   }
+  //   setLoading(false);
 
-  };
+  // };
 
   // loadData will load the users data from SkyDB
   const loadData = async (event) => {
@@ -215,55 +180,62 @@ function App() {
     /************************************************/
     /*        Step 4.5 Code goes here              */
     /************************************************/
+
+    const postsLoader = feedDAC.loadPostsForUser(userID);
+    const firstPage = await postsLoader.next();
+    // console.log(`grabbing items in DAC ${firstPage.value}`);
+
+    posts.push(...firstPage.value);
+    console.log([...posts]);
     // Use getJSON to load the user's information from SkyDB
-    const { data } = await mySky.getJSON(filePath);
-    console.log(data)
+    // const { data } = await mySky.getJSON(filePath);
+    // console.log(data)
     // To use this elsewhere in our React app, save the data to the state.
-    if (data) {
-      setName(data.name);
-      setDescription(data.description);
-      setFileSkylink(data.skylinkUrl);
-      console.log('User data loaded from SkyDB!');
-    } else {
-      console.error('There was a problem with getJSON');
-    }
+    // if (data) {
+    //   setName(data.name);
+    //   setDescription(data.description);
+    //   setFileSkylink(data.skylinkUrl);
+    //   console.log('User data loaded from SkyDB!');
+    // } else {
+    //   console.error('There was a problem with getJSON');
+    // }
     /*****/
 
     setLoading(false);
   };
 
-  const handleSaveAndRecord = async (event) => {
-    event.preventDefault();
-    setLoading(true);
+  // const handleSaveAndRecord = async (event) => {
+  //   event.preventDefault();
+  //   setLoading(true);
 
-    /************************************************/
-    /*        Step 4.6 Code goes here              */
-    /************************************************/
-    console.log('Saving user data to MySky');
+  //   /************************************************/
+  //   /*        Step 4.6 Code goes here              */
+  //   /************************************************/
+  //   console.log('Saving user data to MySky');
 
-    const jsonData = {
-      name,
-      description,
-      skylinkUrl: fileSkylink,
-    };
+  //   const jsonData = {
+  //     name,
+  //     description,
+  //     skylinkUrl: fileSkylink,
+  //   };
 
-    try {
-      // write data with MySky
-      await mySky.setJSON(filePath, jsonData);
+  //   try {
+  //     // write data with MySky
+  //     await mySky.setJSON(filePath, jsonData);
 
-      // Tell contentRecord we updated the color
-      await contentRecord.recordInteraction({
-        skylink: fileSkylink,
-        metadata: { action: 'updatedDescriptionOf' },
-      });
-    } catch (error) {
-      setLoading(false);
-      console.log(`error with setJSON: ${error.message}`);
-    }
-    /*****/
+  //     // Tell contentRecord we updated the color
+  //     await contentRecord.recordInteraction({
+  //       skylink: fileSkylink,
+  //       metadata: { action: 'updatedDescriptionOf' },
+  //     });
+  //   } catch (error) {
+  //     setLoading(false);
+  //     console.log(`error with setJSON: ${error.message}`);
+  //   }
+  //   /*****/
 
-    setLoading(false);
-  };
+  //   setLoading(false);
+  // };
 
   // define args passed to form
   const formProps = {
@@ -271,7 +243,7 @@ function App() {
     handleSubmit,
     handleMySkyLogin,
     handleMySkyLogout,
-    handleSaveAndRecord,
+    // handleSaveAndRecord,
     loadData,
     name,
     description,
